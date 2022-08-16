870bf25898bc8b0dd4e631dfaaa635e8e356f132
Paul Myburgh <lostpebble@protonmail.com>
2022-08-16 13:01:23 +0200

    added text on page to show loaded or not

--- src/main.tsx
+++ src/main.tsx
@@ -2,3 +2,5 @@ import "./config";
 import { Account, multisig } from "near-api-js";
 
 console.log("Testing import and usage", {Account, multisig});
+
+document.getElementById("root").textContent = "loaded script";
